8f4a428e6e5c320288a0d5958756b481d1976a60
Twisted <TheTwistedDev@gmail.com>
2023-07-07 14:35:38 -0700

    styling header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,29 +1,35 @@
 <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js">
-// Your selected Skeleton theme:
-import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
-// This contains the bulk of Skeletons required styles:
-import '@skeletonlabs/skeleton/styles/skeleton.css';
-// Finally, your application's global stylesheet (sometimes labeled 'app.css')
-import '../app.postcss';
-import { AppBar, AppShell } from '@skeletonlabs/skeleton';
-import Icon from '@iconify/svelte';
-import calendarIcon from '@iconify/icons-flat-color-icons/calendar';
-
+    // Your selected Skeleton theme:
+    import '@skeletonlabs/skeleton/themes/theme-hamlindigo.css'
+    // This contains the bulk of Skeletons required styles:
+    import '@skeletonlabs/skeleton/styles/skeleton.css'
+    // Finally, your application's global stylesheet (sometimes labeled 'app.css')
+    import '../app.postcss'
+    import Icon from '@iconify/svelte'
+    import calendarIcon from '@iconify/icons-flat-color-icons/calendar'    
+	import { AppBar, AppShell, LightSwitch } from '@skeletonlabs/skeleton'
 </script>
 
-<AppShell regionPage="relative" slotPageHeader="sticky top-0 z-10" >
-	<svelte:fragment slot="header">  
-    <AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
-        <svelte:fragment slot="lead">
-            <a href="/">
-                <Icon icon={calendarIcon} width="56" height="56" class='cursor-pointer'/>
-            </a>   
-        </svelte:fragment>
-            <h1 class="font-bold text-2xl text-blue-600">Esyncal</h1>
-        <svelte:fragment slot="trail">(actions)</svelte:fragment>
-    </AppBar>
+<svelte:head>
+    <title> Esyncal </title>
+    <meta name="description" content="Next-Gen Calendar app">
+    <meta name="keywords" content="calendar, tasks, todo, app, scheduler, events, synchronization">
+    <meta name="author" content="TheTwistedDev">
+</svelte:head>
+
+<AppShell regionPage="relative" slotPageHeader="sticky top-0 z-10">
+    <svelte:fragment slot="pageHeader">
+        <AppBar>
+            <svelte:fragment slot="lead">
+                <Icon icon={calendarIcon} width="52" height="52" class="ml-8"/>
+            </svelte:fragment>
+            <h1 class="h1 ml-4">Esyncal</h1>
+            <svelte:fragment slot="trail">
+                <LightSwitch />
+                <button type="button" class="btn variant-soft-surface"> Log In </button>
+                <button type="button" class="btn variant-filled-surface"> Create Account </button>
+            </svelte:fragment>
+        </AppBar>
     </svelte:fragment>
-
-	<slot />
-
 </AppShell>
+<slot />
